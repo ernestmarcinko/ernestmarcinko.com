5d471e4df109d39167a0ef392cf961bf8506ce7d
anag0 <ernest.marcinko@gmail.com>
2023-05-06 13:58:33 +0200

    A few minor changes and a readme update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# ernestmarcinko.com
- ernestmarcinko.com
+# Source code of ernestmarcinko.com
+ Hi!
+
+ This is the full source code of my [personal website](https://ernestmarcinko.com). It is super simple, mobile friendly static website and guaranteed to score **100% on google pagespeed insights** - if that's what you are into.
+
+ It's great for very simple presentation style pages, you only need a web server like Apache or Nginx or anything else.
+
+ ## Usage
+  - You download the code
+  - Change it as you like with your favourite editor (or notepad.exe if you are a savage)
+    - Don't forget to remove/replace the Google Tag Manager tracking code from **index.html**
+    - There is a workflow in **.github/workflows/main.yml** to upload the code via SFTP to the production server on push/PR, you may want to remove that too.
+  - Upload to your server
+  - Profit?
+
+## Requirements
+ - Any web server should do the trick
+
+ ## Q & A
+ - Is this simple to use? -> **yes**
+ - Does this require PHP? -> **no**
+ - Does this require Node? -> **no**
+ - Does this require a database? -> **no**
+ - Do I need coding knowledge? -> **Some**, but why not start with this simple project?
+ - Why don't you use classes or any OOP practices in the script?
+    - Because it was simpler this way
+ - You are missing Open Graph, SEO etc.. from the code, what the heck? I need my twitter preview pictures.
+    - Chill bro, this is just a simple presentation website. Feel free to add these as you need them.
+ - What is this good for then? No node.js no PHP, no frameworks, no nothing?
+    - But I only wanted a simple website for myself and share it..   
+ - My friend can do it better and cheaper
+    - But I just... nevermind
+ - You are not even a real programmer
+    - :(
---
+++ site.js
@@ -0,0 +1,48 @@
+/**
+ * I like to wait for the DOMContentLoaded to do my DOM related stuff, but I'm almost
+ * certain this would work without that too, as it's enqueued as last in the footer.
+ */
+window.addEventListener("DOMContentLoaded", () => {
+    let lastKnownScrollPosition = 0;
+    let ticking = false;
+    
+    function highlightProject(scrollPos) {
+        if ( scrollPos > 0 ) {
+            document.querySelectorAll('.project').forEach((el)=>{
+                let top = el.getBoundingClientRect().top;
+                if (  top < 200 && top > -200 ) {
+                    el.classList.add('active');
+                } else {
+                    el.classList.remove('active');
+                }
+            });
+        }
+    }
+
+    function parallaxBg(scrollPos) {
+        let positionY = parseInt(40 + scrollPos / 4);
+        document.querySelector('.main').style.backgroundPositionY = positionY + "px";
+    }
+    
+    /**
+     * Taken from: https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+     * A great way of making sure that the scroll event runs optimally and does not
+     * conflict with the page animation. It's probably unneccessary too, as we do nothing too
+     * fancy, but it still looks nice.
+     */
+    document.addEventListener("scroll", () => {
+        lastKnownScrollPosition = window.scrollY;
+    
+        if ( !ticking ) {
+            window.requestAnimationFrame(() => {
+                if ( typeof screen.orientation !== 'undefined' && window.innerWidth <= 480 ) {
+                    highlightProject(lastKnownScrollPosition);
+                }
+                parallaxBg(lastKnownScrollPosition);
+                ticking = false;
+            });
+        
+            ticking = true;
+        }
+    }, {passive: true});
+});
